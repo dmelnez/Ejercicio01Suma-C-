917c7f1d05a08858f5c9d3aa2fe008068b68be80
DESKTOP-JV3V0TT\dmelnez <csi23-dmelnez@colegioaltair.net>
2023-10-09 14:37:31 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ejercicio01Suma ; 
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+        int numero01;
+        int numero02;
+
+
+
+        Console.WriteLine("Introduzca un Numero");
+        numero01 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Introduzca un Segundo Numero");
+        numero02   = int.Parse(Console.ReadLine());
+        int sumar = numero01 + numero02;
+        Console.WriteLine("El Resultado es: " + sumar);
+        
+    
+    }
+
+
+}
+
+
+
